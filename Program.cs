c7cb9f496d73a9531cb17030cfcfb73ce87c5e02
MEGA <MEGA@gmail.com>
2025-03-20 18:25:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadachi_20._03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> evenStack = new Stack<int>();
+            Stack<int> oddStack = new Stack<int>();
+
+            int N = int.Parse(Console.ReadLine());
+            Console.Write(" генерирани са ");
+
+            Random random = new Random();
+
+            for (int i = 0; i < N; i++)
+            {
+                int num = random.Next(1, 102);
+                if (num %2 ==0)
+                {
+                    evenStack.Push(num);
+                }
+                else
+                {
+                    oddStack.Push(num);
+                }
+            }
+
+            Console.Write("Четни числа: ");
+            while (evenStack.Count > 0)
+            {
+                Console.Write(evenStack.Pop() + " ");
+            }
+
+            Console.Write("Нечетни числа: ");
+            while (oddStack.Count > 0)
+            {
+                Console.Write(oddStack.Pop() + " ");
+            }
+        }
+    }
+}
